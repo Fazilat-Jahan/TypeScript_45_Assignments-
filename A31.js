670fd46f4411a66d16ce980aafb5e9cf83e25681
Fazilat-Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-03 02:52:55 +0500

    Add files via upload

---
+++ A31.js
@@ -0,0 +1,23 @@
+"use strict";
+/* No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+• If the list is empty, print the message We need to find some users!
+• Remove all of the usernames from your array, and make sure the correct message is printed. */
+let users = ["Eric", "Peton", "Dall", "Fari"];
+if (users.length == 0) {
+    console.log("we need to find some users!");
+}
+else {
+    for (let u = 0; u < users.length; u++) {
+        console.log(`Hello ${users[u]}, Thank You for logging in again `);
+    }
+}
+//empty list 
+users = [];
+if (users.length == 0) {
+    console.log("we need to find some users!");
+}
+else {
+    for (let u = 0; u < users.length; u++) {
+        console.log(`Hello ${users[u]}, Thank You for logging in again `);
+    }
+}
