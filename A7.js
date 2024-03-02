2cc3e9f32da29b2b33440d321d7061c97d3eea91
Fazilat-Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-03 02:08:07 +0500

    Add files via upload

---
+++ A7.js
@@ -0,0 +1,8 @@
+"use strict";
+// Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+// You should create four lines that look like this: console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line
+console.log(6 + 2);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
