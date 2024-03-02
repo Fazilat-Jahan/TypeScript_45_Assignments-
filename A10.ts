f66de89336b9e59a3fecc398532188f05987d6bc
Fazilat-Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-03 02:09:43 +0500

    Add files via upload

---
+++ A10.ts
@@ -0,0 +1,21 @@
+
+//Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+//program#1:
+//assinging value in variable to perform airthematic operations:
+let a = 45;
+let b = 55;
+let c = 66;
+let d = 77;
+
+//print all airthematic operations with line braek:
+console.log("Program#1:" , '\n' ,a+b ,'\n' , a-b, '\n' , a/b, '\n' , a*b);
+
+
+//program#2:
+
+//storing string values in variables to print message with person name: 
+let Name: string = "Fazi";
+let message = (`\n "Program#2:" \n Hi! ${Name} Wellcome to the AI world`);
+
+console.log(message);
