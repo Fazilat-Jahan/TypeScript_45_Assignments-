f99b0566f59a57988fca2316eb12592ea67c8ed4
Fazilat-Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-03 02:00:40 +0500

    Question5

---
+++ A5.js
@@ -0,0 +1,5 @@
+"use strict";
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let author = "Martin Luther King .Jr";
+let quote = "We must accept finite disappointment, but never lose infinite hope.";
+console.log(`${author}, Once Said "${quote}"`);
