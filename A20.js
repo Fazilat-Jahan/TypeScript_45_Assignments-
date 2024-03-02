f5afbbbc9d00571c6455b527d048f400052d4fc4
Fazilat-Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-03 02:32:03 +0500

    Add files via upload

---
+++ A20.js
@@ -0,0 +1,7 @@
+"use strict";
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+Object.defineProperty(exports, "__esModule", { value: true });
+let list = ["Books:", "Rich Dad Poor Dad", "Think and Grow Rich",
+    "Hobbies:", "Playing Cricket", "Gardening",
+    "Skills:", "Content Writting", "Amazon PPC Management"];
+list.map((items) => console.log(items));
---
+++ A20.ts
@@ -0,0 +1,7 @@
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let list: string[] =["Books:", "Rich Dad Poor Dad", "Think and Grow Rich",
+ "Hobbies:","Playing Cricket", "Gardening",
+ "Skills:", "Content Writting", "Amazon PPC Management"];
+list.map((items) => console.log(items)
+);
