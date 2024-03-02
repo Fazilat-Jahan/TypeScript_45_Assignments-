c10dd0762b0153ead926b2b8e616bf6226e43f1e
Fazilat-Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-03 02:24:45 +0500

    Add files via upload

---
+++ A11.ts
@@ -0,0 +1,5 @@
+// Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time
+
+let friends:string[] = ["friend1" , "friend2" , "friend3" ];
+console.log(friends)
+console.log(`${friends}`)
