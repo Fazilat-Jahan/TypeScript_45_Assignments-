aacf2cbef4bb4bf4af15b802df960886d3537da6
Fazilat-Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-03 02:27:52 +0500

    Add files via upload

---
+++ A14.js
@@ -0,0 +1,4 @@
+"use strict";
+//If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+let guest = ["Aru", "Sob", "Komi", "Fazi"];
+guest.map((items) => (console.log(`Dear ${items}! You Are Invited at my Home for Dinner.`)));
---
+++ A14.ts
@@ -0,0 +1,5 @@
+//If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+let guest: string[] = ["Aru", "Sob", "Komi","Fazi"]
+
+guest.map((items) => (console.log(`Dear ${items}! You Are Invited At My Home for Dinner.`)) )
