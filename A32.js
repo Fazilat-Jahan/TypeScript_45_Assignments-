b4da4d9935001984d3ecded78fe7cc0b5734d516
Fazilat-Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-03 02:53:20 +0500

    Add files via upload

---
+++ A32.js
@@ -0,0 +1,21 @@
+"use strict";
+/* Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+• Make a list of five or more usernames called current_users.
+• Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+• Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+• Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted. */
+let current_users = ["Fazi", "Hooria", "Aymal", "Aira", "Esra"];
+let new_users = ["Fazi", "Hooria", "Falak", "Namal", "Aina"];
+for (let new_user of new_users) {
+    let isAvailable = true;
+    for (let current_user of current_users) {
+        if (new_user.toLowerCase() == current_user.toLowerCase()) {
+            console.log(`username ${new_user} is already taken.`);
+            isAvailable = false;
+            break;
+        }
+    }
+    if (isAvailable) {
+        console.log(`username ${new_user} is available`);
+    }
+}
