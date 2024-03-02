82168ad64809b65d4221349ad92cb1b4e659d968
Fazilat-Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-03 02:47:15 +0500

    Add files via upload

---
+++ A21.js
@@ -0,0 +1,5 @@
+"use strict";
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let person = { name: "Fazilat Jahan", fname: "M.Younus", education: "DAE in Computer Information Technology",
+    age: 22, profession: "Freelancer" };
+console.log(person);
---
+++ A21.ts
@@ -0,0 +1,9 @@
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+
+
+let person: {name: string, fname:string, education:string, age:number,profession:string} = 
+{ name: "Fazilat Jahan", fname:"M.Younus", education:"DAE in Computer Information Technology",
+age:22, profession: "Freelancer" }
+
+console.log(person)
