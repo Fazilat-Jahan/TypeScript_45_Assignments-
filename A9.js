d6e4ae8616f383fd3e53963844dbcf9b65a284ef
Fazilat-Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-03 02:09:04 +0500

    Add files via upload

---
+++ A9.js
@@ -0,0 +1,4 @@
+
+//Question: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favnum = 2;
+console.log("My Favorite Number Is:", favnum);
