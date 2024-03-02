ad68baf9f6c7a27ef12c9a75e02ed8387b78a043
Fazilat-Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-03 02:30:20 +0500

    Add files via upload

---
+++ A17.ts
@@ -0,0 +1,27 @@
+/* You just found out that your new dinner table won’t arrive in time for the dinner, and you have space for only two guests.
+• Start with your program from Exercise 16. Add a new line that prints a message saying that you can invite only two people for dinner.
+
+• Remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
+
+• Print a message to each of the two people still on your list, letting them know they’re still invited.
+
+• Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program. */
+
+console.log("I've space for only two guest");
+
+let guest: string[] = ["Aru", "Sob", "Nighat", "Fazi", "Komi"];
+
+while(guest.length>2) {
+    let removeGuest = guest.pop()
+    console.log(`Dear! ${removeGuest} Sorry, I can't invited you for dinner`);
+}
+
+guest.map((items) =>
+console.log(`Hey! ${items} You are still Invited for Dinner.`)
+)
+
+guest.pop();
+guest.pop();
+console.log("Guest list at the end",guest);
+
+
