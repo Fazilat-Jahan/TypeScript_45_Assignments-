3a5609ee916311d4b16d10b97eea215aef74adaf
Fazilat-Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-03 02:48:15 +0500

    Add files via upload

---
+++ A23.ts
@@ -0,0 +1,36 @@
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False. 
+
+let test1 = 'Arts'
+console.log("If test1 equals to Arts, I predict True otherwise False, and it is" , test1 == 'Arts')
+
+let test2 = 'mathematics';
+console.log(" If test2 equals to mathematics. I prediuct True otherwise it's False, and it is", test2 == 'engllish' )
+
+let test3 = '55'
+console.log(" If test3 equals to 2. I prediuct True otherwise it's False, and it is", test3 == '2' )
+
+let test4: any = 27
+console.log(" If test4 equals to 27. I prediuct True otherwise it's False, and it is", test4 == 27 )
+
+let test5 = 'Cars'
+console.log(" If test5 equals to cars. I prediuct True otherwise it's False, and it is", test5 == 'Cars' )
+
+let test6 = "language"
+console.log(" If test6 equals to language. I prediuct True otherwise it's False, and it is", test6 == 'translator' )
+
+let test7 = 'test7'
+console.log(" If test7 equals to test6. I prediuct True otherwise it's False, and it is", test7 == 'test6')
+
+let test8: number = 100
+console.log(" If test8 equals to 100. I prediuct True otherwise it's False, and it is", test8 == 99 )
+
+let test9: number = 100
+console.log(" If test9 equals to 100. I prediuct True otherwise it's False, and it is", test9 == 100 )
+
+let test10= 'Fazi'
+console.log(" If test10 equals to Fazi. I prediuct True otherwise it's False, and it is", test10 == 'Fazi')
