869bdb07fb4ea7d8f79819f47ab51b1035ce982e
Fazilat-Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-03 02:30:49 +0500

    Add files via upload

---
+++ A18.ts
@@ -0,0 +1,33 @@
+/* Think of at least five places in the world you’d like to visit.
+• Store the locations in a array. Make sure the array is not in alphabetical order.
+• Print your array in its original order.
+• Print your array in alphabetical order without modifying the actual list.
+• Show that your array is still in its original order by printing it.
+• Print your array in reverse alphabetical order without changing the order of the original list.
+• Show that your array is still in its original order by printing it again.
+• Reverse the order of your list. Print the array to show that its order has changed.
+• Reverse the order of your list again. Print the list to show it’s back to its original order.
+• Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+• Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.*/
+
+let places: string[] = ["Swat", "Kagan",  "Naran", "Kashmir", "Skardu"]
+//printing array in it's original order.
+console.log("Original Order",places)
+//print array in alphabetical order
+console.log("Alphabetical Order",[...places].sort())
+//printing array in it's original order.
+console.log("Original Order",places)
+//print array in reverse alphabetical order
+console.log("Reverse Alphabetical Order",[...places].reverse())
+//printing array in it's original order.
+console.log("Original Order",places)
+//print array in reverse sort alphabetical order
+console.log("Changed Reverse Alphabetical Order",[...places].sort().reverse())
+//print array in again reverse alphabetical order
+console.log("Reverse Alphabetical Order",[...places].reverse())
+//sort array 
+places.sort()
+console.log("Sort Alphabetical Order",places)
+//reverse array
+places.reverse()
+console.log("Reverse Alphabetical Order", places)
