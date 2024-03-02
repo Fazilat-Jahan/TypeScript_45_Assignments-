190ed7479df286a7f6f37ccd7a08a2fa734a9439
Fazilat-Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-03 01:42:55 +0500

    Add files via upload

---
+++ A2.ts
@@ -0,0 +1,4 @@
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+let personame = "Fazi";
+console.log(`Hey ${personame}! ,would you like to learn AI with me from next month?`);
