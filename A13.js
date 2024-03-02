2deb701f14bc72e4fd855f16442f253e60cc934f
Fazilat-Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-03 02:27:12 +0500

    Add files via upload

---
+++ A13.js
@@ -0,0 +1,8 @@
+"use strict";
+// Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+var Cars = ['Honda', 'Civic', 'Mehran', 'Audi', 'Vigo'];
+console.log("I have", Cars[4], "&", Cars[0]);
+console.log("I would like to buy", Cars[3]);
+console.log("I don't like", Cars[2]);
+console.log("I want to gift someone", Cars[1]);
+console.log("I will sell", Cars[0]);
---
+++ A13.ts
@@ -0,0 +1,15 @@
+// Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+var Cars: string[]= ['Honda' , 'Civic' , 'Mehran' , 'Audi' , 'Vigo'];
+
+console.log("I have", Cars[4], "&" , Cars[0]);
+
+console.log("I would like to buy", Cars[3]);
+
+console.log("I don't like", Cars[2]);
+
+console.log("I want to gift someone", Cars[1]);
+
+console.log("I will sell", Cars[0]);
+
+
