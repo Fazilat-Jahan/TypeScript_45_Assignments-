2fa840a3356709a1a19684325459691f49cbfb0e
Fazilat-Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-03 02:47:46 +0500

    Add files via upload

---
+++ A22.js
@@ -0,0 +1,7 @@
+"use strict";
+/* Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program. */
+let error = ["syntax error", "reference error", "type error", "evolution error", "rage error"];
+//I did'nt assinged 5th index in my array 
+console.log(error[5]);
+//I've only 4 index 
+console.log(error[4]);
