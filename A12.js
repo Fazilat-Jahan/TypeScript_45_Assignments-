216dc852f6e27f3b98d136d1977c31a108729c07
Fazilat-Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-03 02:26:29 +0500

    Add files via upload

---
+++ A12.js
@@ -0,0 +1,7 @@
+"use strict";
+//Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let friends = ["Sob!", "Aru!", "Kom!"];
+let message = "Let's Meet ASAP";
+console.log("Hey", friends[0], " " + message);
+console.log("Hey", friends[1], " " + message);
+console.log("Hey", friends[2], " " + message);
