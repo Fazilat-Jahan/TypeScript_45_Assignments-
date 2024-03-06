67dcb5f44dd117cc9c2e60ca3ead1df4f25d27e0
Fazilat Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-06 23:11:30 +0500

    Add files via upload

---
+++ A45.ts
@@ -0,0 +1,18 @@
+// Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
+
+
+function Cars(manufacturer:string, model: string, ...properties:[string, any][]):
+any {
+    const info: any= {
+        manufacturer,
+        model,
+    };
+    for (const [key, value] of properties) {
+        info[key] = value;
+    } 
+    return info;
+}
+
+
+const information: string = Cars("Audi", "S3", ["color", "Red"], ["optional_feature", "Panoramic Sunroof"],["speed", "250km/hr"])
+console.log(information)
