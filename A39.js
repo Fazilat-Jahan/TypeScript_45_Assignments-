93fb75102107585442965955b0204b3c5b3ea48e
Fazilat Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-06 23:05:22 +0500

    Add files via upload

---
+++ A39.js
@@ -0,0 +1,15 @@
+"use strict";
+// City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+// "Lahore, Pakistan"
+// Call your function with at least three city-country pairs, and print the value that’s returned.
+function city_country(city, country) {
+    return `"${city}, ${country}"`;
+}
+//calling function with cities country
+const city1 = city_country("Lohore", "Pakistan");
+const city2 = city_country("London", "UK");
+const city3 = city_country("Vioctoria", "Canada");
+//printing return value
+console.log(city1);
+console.log(city2);
+console.log(city3);
