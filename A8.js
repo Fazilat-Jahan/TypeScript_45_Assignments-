ab4e81197792b52fab88c4984958aaa3521dfe00
Fazilat Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-03-06 22:59:59 +0500

    Add files via upload

---
+++ A8.js
@@ -0,0 +1,8 @@
+"use strict";
+// // You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+console.log(6 + 2);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
